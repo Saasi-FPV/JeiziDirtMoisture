8727291316637881564a11d77dd6970b8ae5235e
Saasi-FPV <82196317+Saasi-FPV@users.noreply.github.com>
2022-06-11 17:06:30 +0200

    Deep Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include <Arduino.h>
 
+
+int TimeToSleep = 10 * 1000000;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(1000);
+  esp_sleep_enable_timer_wakeup(TimeToSleep);
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+  Serial.flush();
+  Serial.println("Bi Wach");
+  delay(1000);
+  Serial.println("Gang ga Schl...ZZZZZZZZZZZ");
+  esp_deep_sleep_start();
+
+
+
+
 }
 
 void loop() {
