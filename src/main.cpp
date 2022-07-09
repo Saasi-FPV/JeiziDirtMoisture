d1e6df7620d0ef3ceae20ccd63745ad509abaa75
Saasi-FPV <82196317+Saasi-FPV@users.noreply.github.com>
2022-07-09 16:44:38 +0200

    sad

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,23 @@ void setup() {
   esp_sleep_enable_timer_wakeup(TimeToSleep);
 
   pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
-  Serial.flush();
+  pinMode(A5, INPUT);
+
   Serial.println("Bi Wach");
+  digitalWrite(2, HIGH);
+
+
+  for(int i = 0; i < 10; i++){
+    float tempVal = analogRead(A5);
+    Serial.print("RAW ADC");
+    Serial.println(tempVal);
+    tempVal = (tempVal * 3.3) / 4096;
+    Serial.print("ADC misst mist?: ");
+    Serial.println(tempVal);
+  }
+
+
+
   delay(1000);
   Serial.println("Gang ga Schl...ZZZZZZZZZZZ");
   esp_deep_sleep_start();
